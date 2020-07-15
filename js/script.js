2d67e7e6ceb29d4e398a5b5bba91aab54d580a31
Marat Bulatov <bulmarik@yandex.ru>
2020-07-16 02:56:08 +0700

    bug fix fonts

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const closeBtn = document.querySelector('.popup__close-btn');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.form');
 const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 const inputName = document.querySelector('.form__input_type_name');
 const inputDescription = document.querySelector('.form__input_type_description');
-const profileDescription = document.querySelector('.profile__description');
 
 function togglePopup() {
     popup.classList.toggle('popup_show');
