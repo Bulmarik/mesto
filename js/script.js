6e1dd1216b1d741909eb5797c44a967d00449330
Marat Bulatov <bulmarik@yandex.ru>
2020-07-18 22:01:21 +0700

    Принято!

--- js/script.js
+++ js/script.js
@@ -7,17 +7,6 @@ const profileDescription = document.querySelector('.profile__description');
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
 
-// То есть вы предлагаете данную функцию не объявлять, а сразу внести в тоггл? 
-// function defaultProfile() {
-//     inputName.value = profileName.textContent;
-//     inputDescription.value = profileDescription.textContent;
-// }
-// Я понял! Мы так делаем потому, что в дальнейшем это не будет задействовано нигде.
-// Поэтому и нет смысла создавать отдельную функцию)
-// Жаль, что мы можем общаться с вами только в итерациях.
-// Очень не хватает практических комментариев в рамках этого обучения.
-// После проверки все комментарии удалю и перезалью на гитхаб :)
-
 function togglePopup() {
     if (popup.classList.contains('popup_show') !== true) {
         inputName.value = profileName.textContent;
