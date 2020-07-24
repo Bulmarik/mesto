5b50227f56530cbb7bba27718099fc1fbef4d02e
Marat Bulatov <bulmarik@yandex.ru>
2020-07-24 19:05:26 +0700

    добавить функционал кнопки добавления мест

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-const editBtn = document.querySelector('.profile__edit-btn');
-const addBtn = document.querySelector('.profile__add-btn');
-const closeBtn = document.querySelector('.popup__close-btn');
-const popup = document.querySelector('.popup');
-const form = document.querySelector('.popup__form');
+const editPopup = document.querySelector('.popup_type_edit-profile');
+const addPopup = document.querySelector('.popup_type_add-card');
+
+const editForm = editPopup.querySelector('.popup__form');
+const addForm = addPopup.querySelector('.popup__form');
+
+const editProfileBtn = document.querySelector('.profile__edit-btn');
+const addCardBtn = document.querySelector('.profile__add-btn');
+
+const editProfileCloseBtn = editPopup.querySelector('.popup__close-btn');
+const addCardCloseBtn = addPopup.querySelector('.popup__close-btn');
+
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
 
-function togglePopup() {
+const inputPlace = document.querySelector('.popup__input_type_place');
+const inputUrl = document.querySelector('.popup__input_type_url');
+
+const cardTemplate = document.querySelector('.template-card').content.querySelector('.elements__item');
+const list = document.querySelector('.elements__items');
+
+const deleteCard = function(evt) {
+  evt.target.closest('.elements__item').remove();
+}
+
+const likeCard = function(evt) {
+  evt.target.classList.toggle('elements__like-btn_active');
+}
+
+function togglePopup(popup) {
   if (!popup.classList.contains('popup_show')) {
     inputName.value = profileName.textContent;
     inputDescription.value = profileDescription.textContent;
@@ -20,13 +42,8 @@ function editProfile(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileDescription.textContent = inputDescription.value;
-  togglePopup();
+  togglePopup(editPopup); 
 }
-
-form.addEventListener('submit', editProfile);
-editBtn.addEventListener('click', togglePopup);
-closeBtn.addEventListener('click', togglePopup);
-addBtn.addEventListener('click', togglePopup);
 
 const initialCards = [
   {
@@ -55,30 +72,50 @@ const initialCards = [
   }
 ];
 
-const cardTemplate = document.querySelector('.template-card').content.querySelector('.elements__item');
-
-const deleteCard = function(evt) {
-  evt.target.closest('.elements__item').remove();
-}
-
-const likeCard = function(evt) {
-  evt.target.classList.toggle('elements__like-btn_active');
-}
-
-initialCards.forEach(function(data) {
-  
+function createCard(data) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector('.elements__image');
   const cardName = cardElement.querySelector('.elements__name');
   const cardLikeBtn = cardElement.querySelector('.elements__like-btn');
   const cardDelBtn = cardElement.querySelector('.elements__del-btn');
-  const list = document.querySelector('.elements__items')
   cardName.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = data.name;
-  list.prepend(cardElement);
+  cardDelBtn.addEventListener('click', deleteCard);
+  cardLikeBtn.addEventListener('click', likeCard);
+  return cardElement;
+}
 
-cardDelBtn.addEventListener('click', deleteCard);
+function renderCard(data) {
+  list.prepend(createCard(data));
+}
 
-cardLikeBtn.addEventListener('click', likeCard);
+initialCards.forEach(function(data) {
+  renderCard(data);
+});
+
+function addCard(evt) {
+  evt.preventDefault();
+  renderCard({name: inputPlace.value, link: inputUrl.value});
+  togglePopup(addPopup); 
+}
+
+editForm.addEventListener('submit', editProfile);
+
+editProfileBtn.addEventListener('click', () => {
+  togglePopup(editPopup)
+});
+
+editProfileCloseBtn.addEventListener('click', () => {
+  togglePopup(editPopup)
+});
+
+addForm.addEventListener('submit', addCard);
+
+addCardBtn.addEventListener('click', () => {
+  togglePopup(addPopup)
+});
+
+addCardCloseBtn.addEventListener('click', () => {
+  togglePopup(addPopup)
 });
