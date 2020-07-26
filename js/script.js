d78da193ff001a9aacbc8f7ac8dd1f8438ee3d1a
Marat Bulatov <bulmarik@yandex.ru>
2020-07-26 07:36:54 +0700

    добавить image popup

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 const editPopup = document.querySelector('.popup_type_edit-profile');
 const addPopup = document.querySelector('.popup_type_add-card');
-
-const editForm = editPopup.querySelector('.popup__form');
-const addForm = addPopup.querySelector('.popup__form');
+const imagePopup = document.querySelector('.popup_type_image');
+const cardTemplate = document.querySelector('.template-card').content.querySelector('.elements__item');
+const list = document.querySelector('.elements__items');
 
 const editProfileBtn = document.querySelector('.profile__edit-btn');
 const addCardBtn = document.querySelector('.profile__add-btn');
 
+const editForm = editPopup.querySelector('.popup__form');
+const addForm = addPopup.querySelector('.popup__form');
+
 const editProfileCloseBtn = editPopup.querySelector('.popup__close-btn');
 const addCardCloseBtn = addPopup.querySelector('.popup__close-btn');
+const imageCloseBtn = imagePopup.querySelector('.popup__close-btn');
 
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
 const inputName = document.querySelector('.popup__input_type_name');
 const inputDescription = document.querySelector('.popup__input_type_description');
-
 const inputPlace = document.querySelector('.popup__input_type_place');
 const inputUrl = document.querySelector('.popup__input_type_url');
 
-const cardTemplate = document.querySelector('.template-card').content.querySelector('.elements__item');
-const list = document.querySelector('.elements__items');
+const imagePopupImg = imagePopup.querySelector('.popup__image');
+const imagePopupFigcapture = imagePopup.querySelector('.popup__figcapture');
 
 const deleteCard = function(evt) {
   evt.target.closest('.elements__item').remove();
@@ -38,37 +41,30 @@ function togglePopup(popup) {
   popup.classList.toggle('popup_show');
 }
 
-function editProfile(evt) {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileDescription.textContent = inputDescription.value;
-  togglePopup(editPopup); 
-}
-
 const initialCards = [
   {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      name: 'Cтадион Медеo',
+      link: './images/medeu_stadium.jpg'
   },
   {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      name: '842 ступени',
+      link: './images/842_steps.jpg'
   },
   {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      name: 'Чимбулак',
+      link: './images/shimbulak.jpg'
   },
   {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      name: 'Канатная дорога',
+      link: './images/funicular_to_shymbulak.jpg'
   },
   {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      name: 'Ущелье Алмарасан',
+      link: './images/almarasan_gorge.jpg'
   },
   {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      name: 'Дорога к Алатау',
+      link: './images/road_to_the_alatau.jpg'
   }
 ];
 
@@ -83,12 +79,21 @@ function createCard(data) {
   cardImage.alt = data.name;
   cardDelBtn.addEventListener('click', deleteCard);
   cardLikeBtn.addEventListener('click', likeCard);
+
+  const clickImage = (evt) => {
+    evt.target.closest('elements__item');
+    imagePopupImg.src = data.link;
+    imagePopupFigcapture.textContent = data.name;
+    togglePopup(imagePopup);
+  }
+
+  cardImage.addEventListener('click', clickImage);
   return cardElement;
 }
 
 function renderCard(data) {
   list.prepend(createCard(data));
-}
+  }
 
 initialCards.forEach(function(data) {
   renderCard(data);
@@ -100,22 +105,34 @@ function addCard(evt) {
   togglePopup(addPopup); 
 }
 
-editForm.addEventListener('submit', editProfile);
+function editProfile(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  togglePopup(editPopup); 
+}
 
 editProfileBtn.addEventListener('click', () => {
   togglePopup(editPopup)
 });
 
-editProfileCloseBtn.addEventListener('click', () => {
-  togglePopup(editPopup)
-});
-
-addForm.addEventListener('submit', addCard);
-
 addCardBtn.addEventListener('click', () => {
   togglePopup(addPopup)
+});
+
+
+editProfileCloseBtn.addEventListener('click', () => {
+  togglePopup(editPopup)
 });
 
 addCardCloseBtn.addEventListener('click', () => {
   togglePopup(addPopup)
 });
+
+imageCloseBtn.addEventListener('click', () => {
+  togglePopup(imagePopup)
+});
+
+editForm.addEventListener('submit', editProfile);
+
+addForm.addEventListener('submit', addCard);
