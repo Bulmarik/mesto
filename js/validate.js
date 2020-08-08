7120597b1a206a86d3b0d60c3ce9751574000baa
Marat Bulatov <bulmarik@yandex.ru>
2020-08-09 00:46:09 +0700

    hotfix вынести функции в global scope

--- js/validate.js
+++ js/validate.js
@@ -7,66 +7,66 @@ const object = {
   errorClass: 'popup__error_visible'
 };
 
-const enableValidation = ({formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass}) => {
-  
+// Я аж сам удивился, до чего получилось разобрать эту "матрёшку" :) Но поначалу это задание мне казалось неосуществимым!
+// Спасибо за хороший опыт!
+
+const enableValidation = (object) => setFormEventListener (object);
+enableValidation(object);
+
+// Функция установки слушателей формы
+function setFormEventListener ({formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass}) {
   const forms = Array.from(document.querySelectorAll(formSelector));
   forms.forEach((formElement) => {
-
-    // Функция установки слушателей на кнопки сабмита форм
     formElement.addEventListener('submit', (evt) => evt.preventDefault());
+    setInputEventLisneners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass);
+  });
+}
 
-    const inputs = Array.from(formElement.querySelectorAll(inputSelector));
-
-    inputs.forEach((inputElement) => {
-
-      // Функция установки слушателей на поля форм
-      inputElement.addEventListener('input', (evt) => {
-
-        // Функция скрытия ошибок полей форм
-        function hideInputError(formElement, inputElement) {
-          const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-          inputElement.classList.remove(inputErrorClass);
-          errorElement.textContent = '';
-          errorElement.classList.remove(errorClass);
-        }
-
-        // Функция показа ошибок полей форм
-        function showInputError(formElement, inputElement) {
-          const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
-          inputElement.classList.add(inputErrorClass);
-          errorElement.textContent = inputElement.validationMessage; 
-          errorElement.classList.add(errorClass);
-        }
-
-        // Функция проверки валидности конкретного поля формы
-        function checkInputValidity(formElement, inputElement) {
-          if (inputElement.validity.valid) {
-            hideInputError(formElement, inputElement); 
-          } else {
-            showInputError(formElement, inputElement);
-          }
-        }
-        checkInputValidity(formElement, inputElement);
-
-        // Функция проверки валидности всех полей
-        const isFormValid = inputs.some((inputElement) => !inputElement.validity.valid);
-
-        // Функция контроля состояния кнопки сабмита форм
-        function toggleButtonState(formElement, isFormValid) {
-          const btnSubmit = formElement.querySelector(submitButtonSelector);
-          if (!isFormValid) {
-            btnSubmit.classList.remove(inactiveButtonClass);
-            btnSubmit.removeAttribute('disabled');
-          } else {
-            btnSubmit.classList.add(inactiveButtonClass);
-            btnSubmit.setAttribute('disabled', true);
-          }
-        }
-        toggleButtonState(formElement, isFormValid);
-
-      });
+// Функция установки слушателей на поля форм
+function setInputEventLisneners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) {
+  const inputs = Array.from(formElement.querySelectorAll(inputSelector));
+  inputs.forEach((inputElement) => {
+    inputElement.addEventListener('input', (evt) => {
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass);
+      toggleButtonState(inputs, formElement, inactiveButtonClass, submitButtonSelector);
     });
   });
 }
 
-enableValidation(object);
+// Функция скрытия ошибок полей форм
+function hideInputError(formElement, inputElement, inputErrorClass, errorClass) {
+  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(errorClass);
+}
+
+// Функция показа ошибок полей форм
+function showInputError(formElement, inputElement, inputErrorClass, errorClass) {
+  const errorElement = formElement.querySelector(`#${inputElement.name}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage; 
+  errorElement.classList.add(errorClass);
+}
+
+/// Функция проверки валидности конкретного поля формы
+function checkInputValidity(formElement, inputElement, inputErrorClass, errorClass) {
+  if (inputElement.validity.valid) {
+    hideInputError(formElement, inputElement, inputErrorClass, errorClass); 
+  } else {
+    showInputError(formElement, inputElement, inputErrorClass, errorClass);
+  }
+}
+
+// Функция контроля состояния кнопки сабмита форм
+function toggleButtonState(inputs, formElement, inactiveButtonClass, submitButtonSelector) {
+  const isFormValid = inputs.some((inputElement) => !inputElement.validity.valid);
+  const btnSubmit = formElement.querySelector(submitButtonSelector);
+  if (!isFormValid) {
+    btnSubmit.classList.remove(inactiveButtonClass);
+    btnSubmit.removeAttribute('disabled');
+  } else {
+    btnSubmit.classList.add(inactiveButtonClass);
+    btnSubmit.setAttribute('disabled', true);
+  }
+}
