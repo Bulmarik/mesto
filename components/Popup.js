d40501291f22e1c2cf20392a799d2dd27a5a97c7
Marat Bulatov <bulmarik@yandex.ru>
2021-01-13 20:04:15 +0700

    class Popup

---
+++ components/Popup.js
@@ -0,0 +1,36 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popupSelector = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popupSelector.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popupSelector.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose() {
+    if(evt.key === 'Escape') {
+      this.close;
+    }
+  }
+
+  _handleOverlayClose(evt) {
+    if (evt.target.classList.contains('popup_opened')) {
+      this.close();
+    };
+  }
+
+  setEventListeners() {
+    const closeBtn = this._popupSelector.querySelector('.popup__close-btn');
+    closeBtn.addEventListener('click', () => {
+      this.close();
+    })
+    this._handleOverlayClose(evt);
+  }
+}  
