bdd18db78716e4717a8cb4f76ea53f91b6359d5a
Marat Bulatov <bulmarik@yandex.ru>
2021-01-15 06:10:28 +0700

    последняя рабочая настройка

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
+import Card from './components/Card.js';
+import FormValidator from './components/FormValidator.js';
 import {validationConfig, addPopup, addForm, addCardBtn, addCardSubmitBtn,
         editPopup, editForm, editProfileBtn, editProfileSubmitBtn,
         imagePopup, cards, templateCard, profileName, profileDescription,
-        inputName, inputDescription, inputPlace, inputUrl} from '../utils/constants.js';
-import initialCards from '../utils/initial-cards.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
+        inputName, inputDescription, inputPlace, inputUrl} from './utils/constants.js';
+import initialCards from './utils/initial-cards.js';
+import Section from './components/Section.js';
+import PopupWithImage from './components/PopupWithImage.js';
+import PopupWithForm from './components/PopupWithForm.js';
+import UserInfo from './components/UserInfo.js';
 
 const addCardValidation = new FormValidator(validationConfig, addPopup);
 addCardValidation.enableValidation();
